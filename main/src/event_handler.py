6d0e00e5ae03d08468d29a61af4f73f6a7f27a10
sethsaperstein <sethsaper@gmail.com>
2020-11-14 14:32:23 -0500

    - include home and away team abbrev in cache

--- main/src/event_handler.py
+++ main/src/event_handler.py
@@ -4,7 +4,7 @@ from queue import Queue
 
 import boto3
 
-from main.src.domain_model import Event
+from main.src.domain_model import Event, StatusDetails
 from main.src.event_status_manager import EventStatusManager
 from main.src.logger import Logger
 from main.src.operator import Operator
@@ -27,18 +27,23 @@ class EventHandler(Operator):
             if event is not None:
                 self.logger.debug("Get event")
                 status = "ACTIVE" if event.winning_team_abbrev is None else "INACTIVE"
-                cached_status = self.esm.get(event.event_id)
+                status_details = StatusDetails(
+                    status=status,
+                    home_team_abbrev=event.home_team_abbrev,
+                    away_team_abbrev=event.away_team_abbrev
+                )
+                cached_status_details = self.esm.get(event.event_id)
 
                 # NEW EVENT to exchange
-                if not cached_status and status == "ACTIVE":
-                    self.handle_new_event(event=event, status=status)
+                if not cached_status_details.status and status_details.status == "ACTIVE":
+                    self.handle_new_event(event=event, status_details=status_details)
                 # EVENT to be removed from exchange and send winner to outgoing kinesis
-                elif cached_status == "ACTIVE" and status == "INACTIVE":
-                    self.handle_inactive_event(event=event, status=status)
+                elif cached_status_details.status == "ACTIVE" and status_details.status == "INACTIVE":
+                    self.handle_inactive_event(event=event, status_details=status_details)
 
-    def handle_new_event(self, event: Event, status: str):
+    def handle_new_event(self, event: Event, status_details: StatusDetails):
         # update cache
-        self.esm.set(event_id=event.event_id, status=status)
+        self.esm.set(event_id=event.event_id, status_details=status_details)
 
         # tell outgoing stream that new event is available
         payload = {
@@ -67,9 +72,9 @@ class EventHandler(Operator):
             self.esm.delete(event_id=event.event_id)
             raise e
 
-    def handle_inactive_event(self, event: Event, status: str):
+    def handle_inactive_event(self, event: Event, status_details: StatusDetails):
         # update cache
-        self.esm.set(event_id=event.event_id, status=status)
+        self.esm.set(event_id=event.event_id, status_details=status_details)
 
         # poison pill the event for the incoming stream so that it cancels bets for that event
         payload = {
@@ -101,5 +106,6 @@ class EventHandler(Operator):
             )
         except Exception as e:
             # set back to active to revert update to cache
-            self.esm.set(event_id=event.event_id, status="ACTIVE")
+            status_details.status = "ACTIVE"
+            self.esm.set(event_id=event.event_id, status_details=status_details)
             raise e
--- main/src/event_status_manager.py
+++ main/src/event_status_manager.py
@@ -1,23 +1,23 @@
 import os
-from typing import Literal
+from main.src.domain_model import StatusDetails
+from json import loads
 
 import redis
 
 
 class EventStatusManager(object):
-    Status = Literal["ACTIVE", "INACTIVE"]
-
     def __init__(self):
         host = os.environ.get("REDIS_HOST")
         port = os.environ.get("REDIS_PORT")
         self._r = redis.StrictRedis(host=host, port=port, db=0)
 
-    def set(self, event_id: str, status: Status):
-        self._r.set(name=f"event:{event_id}", value=status)
+    def set(self, event_id: str, status_details: StatusDetails):
+        self._r.set(name=f"event:{event_id}", value=str(status_details))
 
     def get(self, event_id: str) -> str:
         res = self._r.get(name=f"event:{event_id}")
-        return res.decode("utf-8") if res else None
+        status_details = StatusDetails(**loads(res.decode("utf-8"))) if res else StatusDetails()
+        return status_details
 
     def delete(self, event_id: str):
         self._r.delete(f"event:{event_id}")
