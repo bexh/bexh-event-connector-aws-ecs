9d8c42289a02810d53cbf3c6c2843f70e6842895
sethsaperstein <sethsaper@gmail.com>
2020-11-21 00:28:35 -0500

    - make operators restart on failure - program never exits

--- src/core.py
+++ src/core.py
@@ -1,5 +1,4 @@
 import os
-import timeit
 from queue import Queue
 
 from src.connector import FootballConnector
@@ -13,17 +12,13 @@ class Core:
         self.logger = LoggerFactory().get_logger(name=__name__, log_level=log_level)
 
     def run(self):
-        start = timeit.default_timer()
-
         event_queue = Queue()
 
         football_connector = FootballConnector(logger=self.logger, sink_queue=event_queue)
         event_handler = EventHandler(logger=self.logger, source_queue=event_queue)
 
-        football_connector.daemon = True
-        event_handler.daemon = True
-
         event_handler.start()
         football_connector.start()
 
-        self.logger.info(f"Duration: {timeit.default_timer() - start}")
+        event_handler.join()
+        football_connector.join()
